e49be69ad781073d1a987a01e90a54b3db3493cd
Justin Tormey <justin@blockchain.com>
2017-02-16 22:25:13 -0500

    working login function

--- frontend/src/components/panels/Login.jsx
+++ frontend/src/components/panels/Login.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
-import { compose } from 'ramda'
+import { compose, map, prop } from 'ramda'
 
 import { Wallet } from 'dream-wallet/lib/immutable'
 import { createApi } from 'dream-wallet/lib/network'
 import { decryptWallet } from 'dream-wallet/lib/WalletCrypto'
 
-let link = (ref, p) => (event) => ref.setState({ [p]: event.target.value })
+let link = (that, p) => (event) => that.setState({ [p]: event.target.value })
 
 const styles = {
   input: {
@@ -30,20 +30,12 @@ class Login extends Component {
     let { loadWallet } = this.props
     let { guid, sharedKey, password } = this.state
 
-    let print = (s) => (r) => console.log(s, r) || r
-
-    let login = () => {
+    let login = () => (
       this.api.fetchWalletWithSharedKey(guid, sharedKey)
-        .then(response => JSON.parse(response.payload))
-        .then(print('payload:'))
+        .then(compose(JSON.parse, prop('payload')))
         .then(decryptWallet(password))
-        .then((either) => {
-          either
-            .chain(compose(loadWallet, Wallet))
-            .chain(console.log)
-            .orElse(console.error)
-        })
-    }
+        .then(map(compose(loadWallet, Wallet)))
+    )
 
     return (
       <div>
